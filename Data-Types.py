a646afbf56c2c2c0e7c1d65b9f4f1825dc98e20c
jonvsgithub <jondayizeye537@gmail.com>
2025-12-05 17:26:42 +0200

    Initial commit: Python learning files

---
+++ Data-Types.py
@@ -0,0 +1,12 @@
+#Built-in Data Types
+
+x = 20
+x = 20.5
+x = 1j
+x = ["apple", "banana", "cherry"]
+x = ("apple", "banana", "cherry")
+x = {"name" : "john", "age" : 36}
+x = {"apple",}
+
+x = 1
+print(type(x))
---
+++ python.py
@@ -0,0 +1,43 @@
+# print("hello world")
+
+thislist = ["apple", "banana","cherry"]
+tropical = ["watermellon","pineapple","papaya"]
+thislist.extend(tropical)
+thislist.append("orange")
+thislist.insert(1,"mango")
+print(thislist)
+
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+myvar = "John"
+my_var = "peter"
+myVariableName = "jonas"
+MyVariableName = "Scilla"
+
+x, y, z = "orange", "banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#one value to multiple variables
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#unpack a collection
+
+#fruits = ["apple", "banana", "cherry"]
+x = "python"
+y = "is" 
+z = "awesome"
+print(x , y, z)
+
+
+
