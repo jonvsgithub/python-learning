1ea669c990603f66068086d2cac739dbd85c7e6d
jonvsgithub <jondayizeye537@gmail.com>
2025-12-11 21:43:28 +0200

    Lists

--- Python-Access List Items.py
+++ Python-Access List Items.py
@@ -7,3 +7,21 @@ print(thislist[1])
 thislist = ["apple", "banana", "cherry"]
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list") 
+
+#Change Item Value
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+#Change a Range of Item Values
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+#Insert Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+
